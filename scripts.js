39ccf652dba38c3fd561d43b08fd35f688d3cecd
sivakandan0104 <147966854+sivakandan0104@users.noreply.github.com>
2024-08-29 14:30:57 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Highlight Active Navigation Link
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
